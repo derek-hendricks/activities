70b5704c5f43f9586133e8de400c5b81be8fd6dd
Derek Hendricks <derek.m.hendricks@gmail.com>
2016-11-12 21:08:24 -0700

    sdf

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV != 'production') {
 
 const app = express()
 const router = express.Router();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 var db, flickr;
 
 const parseQuery = (query) => {
